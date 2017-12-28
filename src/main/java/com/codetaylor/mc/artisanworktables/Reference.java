4869503fd77300fe67c421256a3b94a60680c548
Jason Taylor <jason@codetaylor.com>
2017-12-28 15:02:26 -0800

    removed darklands dimension id from reference

--- src/main/java/com/codetaylor/mc/artisanworktables/Reference.java
+++ src/main/java/com/codetaylor/mc/artisanworktables/Reference.java
@@ -2,8 +2,6 @@ package com.codetaylor.mc.artisanworktables;
 
 public class Reference {
 
-  public static final int DIMENSION_ID_DARKLANDS = 10;
-
   public static final String MOD_ID = "artisanworktables";
   public static final String VERSION = "@@VERSION@@";
   public static final String NAME = "Artisan Worktables";
