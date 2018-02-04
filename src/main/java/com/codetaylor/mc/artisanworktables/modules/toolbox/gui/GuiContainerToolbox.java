32d547a2aebf92f93766acc0ade90c9f20246257
Jason Taylor <jason@codetaylor.com>
2018-02-04 12:02:55 -0800

    changed toolbox to use title element

--- src/main/java/com/codetaylor/mc/artisanworktables/modules/toolbox/gui/GuiContainerToolbox.java
+++ src/main/java/com/codetaylor/mc/artisanworktables/modules/toolbox/gui/GuiContainerToolbox.java
@@ -1,10 +1,10 @@
 package com.codetaylor.mc.artisanworktables.modules.toolbox.gui;
 
 import com.codetaylor.mc.athenaeum.gui.GuiContainerBase;
-import com.codetaylor.mc.athenaeum.gui.element.GuiElementTextureRectangle;
+import com.codetaylor.mc.athenaeum.gui.GuiHelper;
 import com.codetaylor.mc.athenaeum.gui.Texture;
-import net.minecraft.client.gui.FontRenderer;
-import net.minecraft.client.resources.I18n;
+import com.codetaylor.mc.athenaeum.gui.element.GuiElementTextureRectangle;
+import com.codetaylor.mc.athenaeum.gui.element.GuiElementTitle;
 
 import java.awt.*;
 
@@ -12,16 +12,30 @@ public class GuiContainerToolbox
     extends GuiContainerBase {
 
   private static final int TEXT_SHADOW_COLOR = new Color(103, 69, 29).getRGB();
-  private final String guiContainerTitleKey;
 
   public GuiContainerToolbox(
       ContainerToolbox container,
-      String guiContainerTitleKey,
+      String titleKey,
       Texture texture
   ) {
 
     super(container, 176, 166);
-    this.guiContainerTitleKey = guiContainerTitleKey;
+
+    // toolbox title
+    this.guiContainerElementAdd(new GuiElementTitle(
+        this,
+        titleKey,
+        8,
+        6
+    ));
+
+    // inventory title
+    this.guiContainerElementAdd(new GuiElementTitle(
+        this,
+        "container.inventory",
+        8,
+        this.ySize - 93
+    ));
 
     this.guiContainerElementAdd(new GuiElementTextureRectangle(
         this,
@@ -42,30 +56,8 @@ public class GuiContainerToolbox
   }
 
   @Override
-  protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
-
-    this.drawString(this.guiContainerTitleKey, 8, 6);
-    this.drawString("container.inventory", 8, this.ySize - 96 + 3);
-
-    super.drawGuiContainerForegroundLayer(mouseX, mouseY);
-  }
-
-  @Override
   public void drawString(String translateKey, int x, int y) {
 
-    String displayText = I18n.format(translateKey);
-    FontRenderer fontRenderer = this.fontRenderer;
-
-    fontRenderer.drawString(displayText, x + 0, y + 1, TEXT_SHADOW_COLOR);
-    fontRenderer.drawString(displayText, x + 1, y + 1, TEXT_SHADOW_COLOR);
-    fontRenderer.drawString(displayText, x + 1, y - 1, TEXT_SHADOW_COLOR);
-    fontRenderer.drawString(displayText, x + 1, y + 0, TEXT_SHADOW_COLOR);
-
-    fontRenderer.drawString(displayText, x - 0, y - 1, TEXT_SHADOW_COLOR);
-    fontRenderer.drawString(displayText, x - 1, y - 1, TEXT_SHADOW_COLOR);
-    fontRenderer.drawString(displayText, x - 1, y + 1, TEXT_SHADOW_COLOR);
-    fontRenderer.drawString(displayText, x - 1, y - 0, TEXT_SHADOW_COLOR);
-
-    fontRenderer.drawString(displayText, x, y, Color.BLACK.getRGB());
+    GuiHelper.drawStringOutlined(translateKey, x, y, this.fontRenderer, TEXT_SHADOW_COLOR);
   }
 }
