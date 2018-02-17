776e0f30f72abe7b2304d17981c0ee09452be54e
Jason Taylor <jason@codetaylor.com>
2018-02-17 12:34:46 -0800

    factored out JEI category factory

--- src/main/java/com/codetaylor/mc/artisanworktables/modules/worktables/integration/jei/PluginJEI.java
+++ src/main/java/com/codetaylor/mc/artisanworktables/modules/worktables/integration/jei/PluginJEI.java
@@ -7,12 +7,13 @@ import com.codetaylor.mc.artisanworktables.modules.worktables.recipe.IRecipe;
 import com.codetaylor.mc.artisanworktables.modules.worktables.recipe.Recipe;
 import com.codetaylor.mc.artisanworktables.modules.worktables.recipe.RegistryRecipe;
 import com.codetaylor.mc.artisanworktables.modules.worktables.reference.EnumTier;
-import mezz.jei.api.*;
-import mezz.jei.api.gui.IDrawable;
+import mezz.jei.api.IJeiRuntime;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.IModRegistry;
+import mezz.jei.api.IRecipeRegistry;
 import mezz.jei.api.recipe.IRecipeCategoryRegistration;
 import mezz.jei.api.recipe.IRecipeWrapper;
 import mezz.jei.api.recipe.transfer.IRecipeTransferRegistry;
-import net.minecraft.util.ResourceLocation;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -27,6 +28,8 @@ public class PluginJEI
   @Override
   public void registerCategories(IRecipeCategoryRegistration registry) {
 
+    JEICategoryFactory categoryFactory = new JEICategoryFactory(registry.getJeiHelpers().getGuiHelper());
+
     for (EnumTier tier : EnumTier.values()) {
 
       if (!ModuleWorktablesConfig.isTierEnabled(tier)) {
@@ -34,7 +37,7 @@ public class PluginJEI
       }
 
       for (String name : WorktableAPI.getWorktableNames()) {
-        registry.addRecipeCategories(this.createCategory(name, tier, registry.getJeiHelpers().getGuiHelper()));
+        registry.addRecipeCategories(categoryFactory.createCategory(name, tier));
       }
     }
   }
@@ -115,91 +118,6 @@ public class PluginJEI
           }
         }
       }
-    }
-  }
-
-  private JEICategoryBase createCategory(
-      String name,
-      EnumTier tier,
-      IGuiHelper guiHelper
-  ) {
-
-    switch (tier) {
-
-      case WORKTABLE:
-        return new JEICategoryWorktable(
-            name,
-            tier,
-            PluginJEI.createUID(name, tier),
-            this.createTitleTranslateKey(name, tier),
-            this.createBackground(name, tier, guiHelper),
-            guiHelper
-        );
-
-      case WORKSTATION:
-        return new JEICategoryWorkstation(
-            name,
-            tier,
-            PluginJEI.createUID(name, tier),
-            this.createTitleTranslateKey(name, tier),
-            this.createBackground(name, tier, guiHelper),
-            guiHelper
-        );
-
-      case WORKSHOP:
-        return new JEICategoryWorkshop(
-            name,
-            tier,
-            PluginJEI.createUID(name, tier),
-            this.createTitleTranslateKey(name, tier),
-            this.createBackground(name, tier, guiHelper),
-            guiHelper
-        );
-
-      default:
-        throw new IllegalArgumentException("Unknown tier: " + tier);
-    }
-  }
-
-  private IDrawable createBackground(String name, EnumTier tier, IGuiHelper guiHelper) {
-
-    if (tier == EnumTier.WORKTABLE) {
-      ResourceLocation resourceLocation = new ResourceLocation(
-          ModuleWorktables.MOD_ID,
-          String.format(ModuleWorktables.Textures.WORKTABLE_GUI, name)
-      );
-      return guiHelper.createDrawable(resourceLocation, 3, 3, 170, 80);
-
-    } else if (tier == EnumTier.WORKSTATION) {
-      ResourceLocation resourceLocation = new ResourceLocation(
-          ModuleWorktables.MOD_ID,
-          String.format(ModuleWorktables.Textures.WORKSTATION_GUI, name)
-      );
-      return guiHelper.createDrawable(resourceLocation, 3, 3, 170, 102);
-
-    } else if (tier == EnumTier.WORKSHOP) {
-      ResourceLocation resourceLocation = new ResourceLocation(
-          ModuleWorktables.MOD_ID,
-          String.format(ModuleWorktables.Textures.WORKSHOP_GUI, name)
-      );
-      return guiHelper.createDrawable(resourceLocation, 3, 13, 206, 118);
-
-    } else {
-      throw new IllegalArgumentException("Unknown tier: " + tier);
-    }
-  }
-
-  private String createTitleTranslateKey(String name, EnumTier tier) {
-
-    switch (tier) {
-      case WORKTABLE:
-        return String.format(ModuleWorktables.Lang.WORKTABLE_TITLE, name);
-      case WORKSTATION:
-        return String.format(ModuleWorktables.Lang.WORKSTATION_TITLE, name);
-      case WORKSHOP:
-        return String.format(ModuleWorktables.Lang.WORKSHOP_TITLE, name);
-      default:
-        throw new IllegalArgumentException("Unknown tier: " + tier);
     }
   }
 
