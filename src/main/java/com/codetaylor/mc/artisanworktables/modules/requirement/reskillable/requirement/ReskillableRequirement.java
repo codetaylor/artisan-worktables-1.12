0e2f9fceed9bbd9d96d5b864564ddb790a13f0bc
Jason Taylor <jason@codetaylor.com>
2018-03-24 13:37:21 -0700

    Hack reskillable requirement #38
    This commit adds Reskillable's method of testing requirements directly to the ReskillableRequirement class. This was done to test that integration with the mod will work once they get their end sorted.
    
    This code should not remain longer than absolutely necessary as it is hacky and is hard-coded for only two types of requirements. It will not work as expected if more requirement types are added.

--- src/main/java/com/codetaylor/mc/artisanworktables/modules/requirement/reskillable/requirement/ReskillableRequirement.java
+++ src/main/java/com/codetaylor/mc/artisanworktables/modules/requirement/reskillable/requirement/ReskillableRequirement.java
@@ -1,6 +1,13 @@
 package com.codetaylor.mc.artisanworktables.modules.requirement.reskillable.requirement;
 
+import codersafterdark.reskillable.api.data.PlayerData;
+import codersafterdark.reskillable.api.data.PlayerSkillInfo;
 import codersafterdark.reskillable.api.data.RequirementHolder;
+import codersafterdark.reskillable.api.requirement.Requirement;
+import codersafterdark.reskillable.api.requirement.SkillRequirement;
+import codersafterdark.reskillable.api.requirement.TraitRequirement;
+import codersafterdark.reskillable.api.skill.Skill;
+import codersafterdark.reskillable.api.unlockable.Unlockable;
 import com.codetaylor.mc.artisanworktables.api.recipe.requirement.IRequirement;
 import com.codetaylor.mc.artisanworktables.modules.requirement.reskillable.ModuleRequirementReskillable;
 import net.minecraft.util.ResourceLocation;
@@ -30,6 +37,34 @@ public class ReskillableRequirement
   @Override
   public boolean match(ReskillableRequirementContext context) {
 
-    return context.getPlayerData().matchStats(this.requirementHolder);
+    PlayerData playerData = context.getPlayerData();
+
+    for (Requirement requirement : this.requirementHolder.getRequirements()) {
+
+      if (requirement instanceof SkillRequirement) {
+        Skill requirementSkill = ((SkillRequirement) requirement).getSkill();
+        int requirementLevel = ((SkillRequirement) requirement).getLevel();
+
+        PlayerSkillInfo playerSkillInfo = playerData.getSkillInfo(requirementSkill);
+        int playerSkillInfoLevel = playerSkillInfo.getLevel();
+
+        if (playerSkillInfoLevel < requirementLevel) {
+          return false;
+        }
+
+      } else if (requirement instanceof TraitRequirement) {
+        Unlockable requirementUnlockable = ((TraitRequirement) requirement).getUnlockable();
+        Skill requirementParentSkill = requirementUnlockable.getParentSkill();
+
+        PlayerSkillInfo playerSkillInfo = playerData.getSkillInfo(requirementParentSkill);
+
+        if (!playerSkillInfo.isUnlocked(requirementUnlockable)) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+    //return context.getPlayerData().matchStats(this.requirementHolder);
   }
 }
