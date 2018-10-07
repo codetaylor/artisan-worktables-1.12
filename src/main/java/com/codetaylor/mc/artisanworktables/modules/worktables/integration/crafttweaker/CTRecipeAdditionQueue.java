0e93db0370f400ae563c9d8ee5a2f0cbb09b2f8d
Jason Taylor <jason@codetaylor.com>
2018-10-07 12:41:42 -0700

    Fix #132

--- src/main/java/com/codetaylor/mc/artisanworktables/modules/worktables/integration/crafttweaker/CTRecipeAdditionQueue.java
+++ src/main/java/com/codetaylor/mc/artisanworktables/modules/worktables/integration/crafttweaker/CTRecipeAdditionQueue.java
@@ -3,11 +3,10 @@ package com.codetaylor.mc.artisanworktables.modules.worktables.integration.craft
 import com.codetaylor.mc.artisanworktables.api.internal.recipe.RecipeBuilderException;
 import com.codetaylor.mc.artisanworktables.modules.worktables.ModuleWorktables;
 import com.codetaylor.mc.artisanworktables.modules.worktables.recipe.IRecipeAdditionQueue;
-import com.codetaylor.mc.artisanworktables.modules.worktables.recipe.copy.IRecipeBuilderCopyStrategyInternal;
 import com.codetaylor.mc.artisanworktables.modules.worktables.recipe.RecipeBuilderInternal;
+import com.codetaylor.mc.artisanworktables.modules.worktables.recipe.copy.IRecipeBuilderCopyStrategyInternal;
 import crafttweaker.mc1120.CraftTweaker;
 import crafttweaker.mc1120.events.ScriptRunEvent;
-import crafttweaker.mc1120.recipes.MCRecipeManager;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.util.ArrayList;
@@ -34,55 +33,31 @@ public class CTRecipeAdditionQueue
   @SubscribeEvent
   public void onScriptEvent(ScriptRunEvent.Post event) {
 
-    // Hacks!
+    // This is intended to run immediately after CrT executes its scripts.
 
-    // This is a hack intended to hook CraftTweaker and run code immediately before
-    // CT processes recipe removals. At this point, we have access to the same
-    // recipes that CT does and can safely initialize any copy recipes.
+    // initialize copy recipes
+    for (RecipeBuilderInternal builder : CTRecipeAdditionQueue.this.recipeBuilderWithCopyList) {
 
-    // Running this immediately before CT removes any recipes allows us to process
-    // our recipe copy actions while the recipes still exist and after all recipes
-    // 'should' be loaded. After the recipe is copied, the builder is validated and
-    // added to CT's LATE_ACTIONS collection, which is processed after all recipe
-    // additions and removals.
+      try {
+        IRecipeBuilderCopyStrategyInternal recipeCopyStrategy = builder.getRecipeCopyStrategy();
 
-    MCRecipeManager.recipesToRemove.add(0, new MCRecipeManager.ActionBaseRemoveRecipes() {
-
-      @Override
-      public void apply() {
-
-        // initialize copy recipes
-        for (RecipeBuilderInternal builder : CTRecipeAdditionQueue.this.recipeBuilderWithCopyList) {
-
-          try {
-            IRecipeBuilderCopyStrategyInternal recipeCopyStrategy = builder.getRecipeCopyStrategy();
-
-            if (recipeCopyStrategy != null) {
-              // execute copy strategy
-              recipeCopyStrategy.apply(builder, CTRecipeAdditionQueue.this.recipeBuilderList);
-            }
-
-          } catch (RecipeBuilderException e) {
-            ModuleWorktables.LOG.error("", e);
-          }
+        if (recipeCopyStrategy != null) {
+          // execute copy strategy
+          recipeCopyStrategy.apply(builder, CTRecipeAdditionQueue.this.recipeBuilderList);
         }
 
-        for (RecipeBuilderInternal builder : CTRecipeAdditionQueue.this.recipeBuilderList) {
-
-          CraftTweaker.LATE_ACTIONS.add(new CTActionAdd(builder.getTableName(), builder));
-        }
-
-        CTRecipeAdditionQueue.this.recipeBuilderWithCopyList.clear();
-        CTRecipeAdditionQueue.this.recipeBuilderList.clear();
+      } catch (RecipeBuilderException e) {
+        ModuleWorktables.LOG.error("", e);
       }
+    }
 
-      @Override
-      public String describe() {
+    for (RecipeBuilderInternal builder : CTRecipeAdditionQueue.this.recipeBuilderList) {
 
-        return "ArtisanWorktables recipe hook queue";
-      }
-    });
+      CraftTweaker.LATE_ACTIONS.add(new CTActionAdd(builder.getTableName(), builder));
+    }
 
+    CTRecipeAdditionQueue.this.recipeBuilderWithCopyList.clear();
+    CTRecipeAdditionQueue.this.recipeBuilderList.clear();
   }
 
 }
